274bb7e492a56c7a3eac404f5190337f4891d0b0
OctavianGheorghiu <127142875+OctavianGheorghiu@users.noreply.github.com>
2025-12-28 19:01:44 +0200

    Initial commit

---
+++ src/main/java/ro/ase/subscriptionshub/SubscriptionsHub.java
@@ -0,0 +1,11 @@
+package ro.ase.subscriptionshub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionsHub {
+    public static void main(String[] args) {
+        SpringApplication.run(SubscriptionsHub.class, args);
+    }
+}
